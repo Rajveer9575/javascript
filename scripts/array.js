e7c3ef6aac8b997fdb4ef994e72aa8ff5eb18b85
Rajveer9575 <rajveersc70@gmail.com>
2021-08-20 17:45:35 +0530

    Get Fruit Button added

--- scripts/array.js
+++ scripts/array.js
@@ -29,3 +29,39 @@ let matrix = [
 let num = ["I","go","home"];
 num.splice(0,3,"Let's dance","right","now");
 console.log(num);
+
+/** one step from the end */
+num.splice(-1,0, 3,4);
+console.log(num);
+
+let alpha = ['t','e','s','t'];
+console.log(alpha.slice(1,3));
+
+let alpha1 = [];
+alpha1 = alpha1.concat(num,alpha);
+console.log(alpha1);
+
+let alpha2 = [1,2];
+
+let  arrayLike = {
+    0:"something",
+    1: "else",
+    [Symbol.isConcatSpreadable]:true,
+    length:2
+};
+
+console.log(alpha2.concat(arrayLike));
+
+/** iterate forEach method */
+
+// alpha1.forEach((item,index,alpha1)=>{
+//   console.log(`${item} is at index ${index} in ${alpha1}`)
+// });
+
+const getFruits = ()=> {
+    fruits.forEach((item,index,fruits) => {
+        console.log(`${item} is at index ${index} in ${fruits}`);
+    });
+}
+
+getFruits();
